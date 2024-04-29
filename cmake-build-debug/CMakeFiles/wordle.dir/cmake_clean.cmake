c00d101509d9cea62e5903dd7768f98b0de3b6ac
JaneBabiakA <janefrances369@gmail.com>
2024-04-29 14:37:07 -0400

    Finished basic wordle functionality.

---
+++ cmake-build-debug/CMakeFiles/wordle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wordle.dir/main.cpp.o"
+  "CMakeFiles/wordle.dir/main.cpp.o.d"
+  "wordle"
+  "wordle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wordle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 
 int main() {
+    //TODO: check that guesses are valid words
+    //TODO: choose random words from a dictionary
+    //TODO: figure out command parsing/flow for tool
     std::string word = "crane";
-    std::string guess;
-    std::cin >> guess;
-    for(char letter = 0; letter <= guess.length(); letter++){
-        if(guess[letter] == word[letter]){
-            std::cout << guess[letter];
-            std::cout << "^[35mTexting^[0m";
-
+    int attempts = 0;
+    while(attempts < 6){
+        std::string guess = "crane";
+        if(guess.length() != 5){
+            std::cout << "Enter five letters." << std::endl;
         }
         else{
-            std::cout << "*";
+            for(int letter = 0; letter < guess.length(); letter++){
+                if(guess[letter] == word[letter]){
+                    std::cout  << "\033[1;32m" << guess[letter] << "\033[0m";
+                }
+                else if((word.find(guess[letter]) != std::string::npos)){
+                    std::cout  << "\033[1;33m" << guess[letter] << "\033[0m";
+                }
+                else{
+                    std::cout << guess[letter];
+                }
+            }
+            std::cout << std::endl;
+            if(guess == word){
+                std::cout << "Congrats!";
+                break;
+            }
+            attempts++;
+            if(attempts == 6){
+                std::cout << "Better luck next time.";
+            }
         }
     }
     return 0;
